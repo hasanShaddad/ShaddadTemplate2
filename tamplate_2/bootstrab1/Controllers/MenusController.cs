10d6e9fd3694b514b2aa13233c22d2e8112a99e5
egyptioncoder <egyptioncoder@gmail.com>
2016-04-10 02:04:26 +0200

    mvc fully dynamic and responsive web site with control panel
    mvc fully dynamic and responsive web site with control panel
    with tow  database .bak  just restore the databases and edit the
    connection string in webconfig and enjoy it

---
+++ tamplate_2/bootstrab1/Controllers/MenusController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI;
+using Tamplate_2.Models;
+
+namespace Tamplate_2.Controllers
+{
+    public class MenusController : Controller
+    {
+        //
+        // GET: /Menus/
+        private ShaddadDbEntities db = new ShaddadDbEntities();
+         //[OutputCache(Duration = 360)]
+        public ActionResult Menu()
+        {
+            var roles = new List<string> { "Admin", "Developer" };
+            var userRoles = Roles.GetRolesForUser(User.Identity.Name);
+
+            if (userRoles.Any(u => roles.Contains(u)))
+            {
+                ViewBag.auth = "admin";
+            }
+            else
+            {
+                ViewBag.auth = "user";
+            }
+            var C_Section_Table = db.C_Section_Table;
+            return PartialView(C_Section_Table.ToList());
+        }
+
+    }
+}
---
+++ tamplate_2/bootstrab1/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bootstrab1.MvcApplication" Language="C#" %>
